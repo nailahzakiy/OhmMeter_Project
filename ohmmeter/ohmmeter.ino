2605db7ea26abf758aa7b8bb9e0b8d3002b8876c
Cyanchlor <nailahz41@gmail.com>
2025-02-18 17:54:01 +0700

    Update ohmmeter.ino
    Commit message
    Co-authored-by: Schnellzen
    
    Co-Authored-By: Schnellzen <160202065+schnellzen@users.noreply.github.com>

--- ohmmeter/ohmmeter.ino
+++ ohmmeter/ohmmeter.ino
@@ -7,11 +7,13 @@ float Rref = 99999;
 float R = 0;
 float val;
 
+//void setup
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
+//void loop
 void loop() {
   // put your main code here, to run repeatedly:
   sensorValue =  analogRead(sensorPin);
